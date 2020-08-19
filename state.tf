76db01aafe1a3012af003734aa809c5bda0a8e8e
Ubuntu <ubuntu@ip-172-31-76-73.ec2.internal>
2020-08-19 01:25:05 +0000

    Correcoes pontuais para desativar webhook

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-12345"
+    bucket = "hackaton-fiap-35scj-335826"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
